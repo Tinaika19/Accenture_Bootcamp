44207ded80e6f0166b83b12238b62cc305bf3f7a
William Fernandes <william.fernandes@kloud.com.au>
2024-07-02 22:50:06 +1000

    new files added

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,4 +1,15 @@
-// JavaScript to toggle the active class on click
-document.querySelector('.menu-icon').addEventListener('click', function() {
-    this.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function() {
+    const bottomTextButton = document.getElementById('bottom-text');
+
+    bottomTextButton.addEventListener('click', function() {
+        window.location.href = 'services.html';
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const continueButton = document.getElementById('continueButton');
+    if (continueButton) {
+        continueButton.addEventListener('click', function() {
+            window.location.href = 'feedback.html';
+        });
+    }
 });
